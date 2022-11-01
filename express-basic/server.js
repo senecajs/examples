140e8405562220e1f01a877108cb9d9286f8b74d
christiangubana <christiangubana@gmail.com>
2022-11-01 15:36:18 +0200

    Update PR - round 2

--- express-basic/server.js
+++ express-basic/server.js
@@ -1,39 +1,43 @@
-
 // Notes.
 // Use standardjs style: https://standardjs.com/
 
+const Express = require("express");
+const Seneca = require("seneca");
 
-const Express = require('express')
-const Seneca = require('seneca')
-
-
-const app = Express()
-
+const app = Express();
 
 // TODO:
 // 1. extend to support red, green, blue - generate PR
 
+app.use("/color/:name", function (req, res) {
+  let name = req.params.name;
+  console.log("NAME", name);
 
-app.use('/color/:name', function(req, res) {
-  let name = req.params.name
-  console.log('NAME', name)
+  let code = "#CCCCCC";
 
-  let code = '#CCCCCC'
-  
   if('red' === name) {
     code = '#FF0000'
   } else if('green' === name) {
     code = '#00ff00'
-  } else if ('blue' === name) {
+  } else if('blue' === name) {
     code = '#0000ff'
   }
 
-  res.send(code)
-})
+  //Using switch statement.
+  // switch (name) {
+  //   case "red":
+  //     code = "#FF0000";
+  //     break;
+  //   case "green":
+  //     code = "#00ff00";
+  //     break;
+  //   case "blue":
+  //     code = "#0000ff";
+  //     break;
+  //   default:
+  //     break;
+  // }
+  res.send(code);
+});
 
-app.listen(3000)
-
-
-
-
-
+app.listen(3000);
